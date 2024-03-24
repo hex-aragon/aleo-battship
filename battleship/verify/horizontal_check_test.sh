acf800fde36fab1d8fb7f875b9153b04132ac835
hex-aragon <0xaragon90@gmail.com>
2024-03-25 06:51:06 +0900

    EDIT: unused battleship

---
+++ battleship/verify/horizontal_check_test.sh
@@ -0,0 +1,3 @@
+echo "인접한 뒤집힌 비트가 행을 나누지 않는지 체크"
+echo "is_horizontal: bool = horizontal_check(ship, horizontal)"
+leo run horizontal_check_test 34084860461056u64 31u64
--- zkbattle_ship_robert_23504/deploy.sh
+++
@@ -1,8 +0,0 @@
-# #!/bin/bash
-
-APPNAME="zkbattle_ship_robert_23504"
-ENV_FILE=".env" 
-PRIVATE_KEY=$(grep "PRIVATE_KEY" $ENV_FILE | cut -d '=' -f2)
-
-
-snarkos developer deploy "${APPNAME}.aleo" --path "./build/" --private-key "${PRIVATE_KEY}" --query "https://api.explorer.aleo.org/v1" --priority-fee 9000000 --broadcast "https://api.explorer.aleo.org/v1/testnet3/transaction/broadcast" \
